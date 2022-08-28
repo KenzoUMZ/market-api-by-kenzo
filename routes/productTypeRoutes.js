a8409fa5fcc6e3a4f96be954333ce792598be6b4
KenzoUMZ <kenzoumezawa@gmail.com>
2022-08-28 13:31:21 -0300

    fix: alphabetically sort

--- routes/productTypeRoutes.js
+++ routes/productTypeRoutes.js
@@ -21,12 +21,11 @@ const productTypeRoute = (app) => {
     })
     app.get('/productType', async (req, res) => {
         try {
-           
-            await ProductType.find().collation({ locale: 'en', strength: 2 }).sort({ name: 1 }).then((list) =>
-                res.status(200).json(list)
-            );
+
+            const list = await ProductType.find().collation({ locale: 'en', strength: 2 }).sort({ name: 1 });
 
 
+            res.status(200).json(list);
         } catch (error) {
             res.status(500).json({ erro: error })
         }
