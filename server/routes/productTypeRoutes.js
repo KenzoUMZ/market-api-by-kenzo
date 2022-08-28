3d09599800baf512851fdc7b9007c6a0a456b28c
KenzoUMZ <kenzoumezawa@gmail.com>
2022-08-28 13:13:04 -0300

    feat:sort product types alphabetically

--- server/routes/productTypeRoutes.js
+++ server/routes/productTypeRoutes.js
@@ -21,9 +21,11 @@ const productTypeRoute = (app) => {
     })
     app.get('/productType', async (req, res) => {
         try {
-            const list = await ProductType.find()
+            const list = await ProductType.find().collation({ locale: 'en', strength: 2 }).sort({ name: 1 }).then((list) =>
+                res.status(200).json(list)
+            );
 
-            res.status(200).json(list)
+
         } catch (error) {
             res.status(500).json({ erro: error })
         }
